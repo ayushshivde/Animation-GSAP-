bc1f8153a2b5f5e8d464abb61a20ad8fbbeb1ad3
Ayush shivde <shivdeayush09@gmail.com>
2025-08-04 00:22:17 +0530

    second commit

--- 3d image zoom/Nothing_hackathon_project - main - Copy - Copy/src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const About = () => {
-  return (
-    <div className='flex w-screen h-screen justify-center items-center text-white'>About</div>
-  )
-}
-
-export default About
